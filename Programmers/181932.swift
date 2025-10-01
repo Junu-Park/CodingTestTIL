3cfb41d292792f75026d6d8455182a43759f08bc
Park Junwoo <ays99111@naver.com>
2025-10-01 17:47:35 +0900

    프로그래머스 코드 처리하기(181932) 풀이 개선

--- Programmers/181932.swift
+++ Programmers/181932.swift
@@ -1,19 +1,18 @@
 // 코드 처리하기(181932)
 
 func solution(_ code:String) -> String {
-    let arr = code.map { String($0) }
     var answer = ""
     var mode = false
-    for i in arr.indices {
-        if arr[i] == "1" {
+    for idx, char in code.enumerated() {
+        if char == "1" {
             mode.toggle()
             continue
         }
         
-        if mode, i % 2 != 0 {
-            answer += arr[i]
-        } else if !mode, i % 2 == 0 {
-            answer += arr[i]
+        if mode, idx % 2 != 0 {
+            answer += char
+        } else if !mode, idx % 2 == 0 {
+            answer += char
         }
     }
     
