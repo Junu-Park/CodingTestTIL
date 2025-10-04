e4e95e991e548586d383141ef6fdcf308e89e114
Park Junwoo <ays99111@naver.com>
2025-10-04 15:55:20 +0900

    모스부호(1)(120838) 추가 풀이

--- Programmers/120838.swift
+++ Programmers/120838.swift
@@ -19,3 +19,15 @@ func solution(_ letter:String) -> String {
 
     return answer
 }
+
+func solution2(_ letter:String) -> String {
+    let morse = [
+    ".-":"a","-...":"b","-.-.":"c","-..":"d",".":"e","..-.":"f",
+    "--.":"g","....":"h","..":"i",".---":"j","-.-":"k",".-..":"l",
+    "--":"m","-.":"n","---":"o",".--.":"p","--.-":"q",".-.":"r",
+    "...":"s","-":"t","..-":"u","...-":"v",".--":"w","-..-":"x",
+    "-.--":"y","--..":"z"
+    ]
+
+    return letter.split(separator: " ").compactMap { morse[String($0)] }.joined()
+}
