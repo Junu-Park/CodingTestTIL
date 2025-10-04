c59bca32736e072d61613644e0f66f76fb3b9c7e
Park Junwoo <ays99111@naver.com>
2025-10-04 15:27:23 +0900

    순서쌍의 개수(120836) 추가 풀이

--- Programmers/120836.swift
+++ Programmers/120836.swift
@@ -11,3 +11,18 @@ func solution(_ n:Int) -> Int {
 
     return answer
 }
+
+// 제곱근 활용
+func solution2(_ n:Int) -> Int {
+    var answer = 0
+
+    let squareRoot = Int(sqrt(Double(n)))
+
+    for i in 1...squareRoot {
+        if n % i == 0 {
+            answer += (i * i == n) ? 1 : 2
+        }
+    }
+
+    return answer
+}
