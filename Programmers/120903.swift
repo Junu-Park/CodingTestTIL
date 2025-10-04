9a60ef1e9fc1a39880a393cc6391f6d9eaeaa549
Park Junwoo <ays99111@naver.com>
2025-10-04 15:10:59 +0900

    프로그래머스 배열의 유사도(120903)

---
+++ Programmers/120903.swift
@@ -0,0 +1,19 @@
+// 배열의 유사도(120903)
+
+func solution(_ s1:[String], _ s2:[String]) -> Int {
+    var s1Dic: [String: Void] = [:]
+
+    for s in s1 {
+        s1Dic[s] = ()
+    }
+
+    var answer = 0
+
+    for s in s2 {
+        if s1Dic[s] != nil {
+            answer += 1
+        }
+    }
+
+    return answer
+}
