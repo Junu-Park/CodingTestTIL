6385148849adf0997e6c6bb929d5d8d75b078f73
Park Junwoo <ays99111@naver.com>
2025-10-05 17:39:15 +0900

    공 던지기(120843) 풀이 수정

--- Programmers/120843.swift
+++ Programmers/120843.swift
@@ -1,13 +1,7 @@
 // 공 던지기(120843)
 
 func solution(_ numbers:[Int], _ k:Int) -> Int {
-    var idx = 0 + (k - 1) * 2
-
-    let count = numbers.count
-
-    if idx >= count {
-        idx = idx % count
-    }
-
-    return numbers[idx]
+    let idx = 0 + (k - 1) * 2
+    
+    return numbers[idx % numbers.count]
 }
