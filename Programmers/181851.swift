24dd6f76ce717df63484a55b7c4c33659746de82
Park Junwoo <ays99111@naver.com>
2025-10-18 14:30:50 +0900

    프로그래머스 전국 대회 선발 고사(181851) 추가 풀이

--- Programmers/181851.swift
+++ Programmers/181851.swift
@@ -27,3 +27,31 @@ func solution2(_ rank: [Int], _ attendance: [Bool]) -> Int {
 
     return result[0].offset * 10000 + result[1].offset * 100 + result[2].offset
 }
+
+// 힙 방식 활용
+func solution3(_ rank: [Int], _ attendance: [Bool]) -> Int {
+    var result: [(index: Int, rank: Int)] = []
+
+    for (idx, rk) in rank.enumerated() {
+        if !attendance[idx] { continue }
+        
+        if result.count < 3 {
+            result.append((idx, rk))
+            if result.count == 3 {
+                result.sort { $0.rank < $1.rank }
+            }
+        } else if rk < result[2].rank {
+            result[2] = (idx, rk)
+
+            if rk < result[1].rank {
+                result.swapAt(1, 2)
+
+                if rk < result[0].rank {
+                    result.swapAt(0, 1)
+                }
+            }
+        }
+    }
+    
+    return result[0].index * 10000 + result[1].index * 100 + result[2].index
+}
