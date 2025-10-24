2a6976193e8d2ba5e50ac98064101f05b1f6a10e
Park Junwoo <ays99111@naver.com>
2025-10-24 15:38:50 +0900

    프로그래머스 영어가 싫어요(120894) 풀이

---
+++ Programmers/120894.swift
@@ -0,0 +1,18 @@
+// 영어가 싫어요(120894)
+
+func solution(_ numbers:String) -> Int64 {
+    let dict = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+
+    var stack: [Character] = []
+
+    var answer = ""
+    for n in numbers {
+        stack.append(n)
+        if let num = dict[String(stack)] {
+            answer += String(num)
+            stack.removeAll()
+        }
+    }
+
+    return Int64(answer)!
+}
