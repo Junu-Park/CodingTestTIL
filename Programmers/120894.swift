8b34f0c95c5de7a71f35f09059400697a0ac600c
Park Junwoo <ays99111@naver.com>
2025-10-24 15:51:19 +0900

    프로그래머스 영어가 싫어요(120894) 추가 풀이

--- Programmers/120894.swift
+++ Programmers/120894.swift
@@ -14,3 +14,16 @@ func solution(_ numbers:String) -> Int64 {
 
     return Int64(answer)!
 }
+
+// 1번 방법(O(n))보다 느림(O(n * k))
+// k = dict의 길이, n = 주어진 문자열의 길이
+func solution2(_ numbers:String) -> Int64 {
+    let dict = ["zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"]
+    var result = numbers
+
+    for (key, value) in dict {
+        result = result.replacingOccurrences(of: key, with: value)
+    }
+    
+    return Int64(result)!
+}
