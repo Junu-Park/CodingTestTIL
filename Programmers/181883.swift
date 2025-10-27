4443c23f5d8c04d8d01fd94d8a1bcd9b817c17b8
Park Junwoo <ays99111@naver.com>
2025-10-27 10:20:46 +0900

    프로그래머스 수열과 구간 쿼리1(181883) 풀이 개선

--- Programmers/181883.swift
+++ Programmers/181883.swift
@@ -2,13 +2,10 @@
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var answer = arr
-    let count = arr.count
     
     for q in queries {
-        for i in 0..<count {
-            if i >= q[0] && i <= q[1]  {
-                answer[i] += 1
-            }
+        for i in q[0]...q[1] {
+            answer[i] += 1
         }
     }
     
