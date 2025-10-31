36a03be88206976af33a26a7c48ab8cfe41bb5d8
Park Junwoo <ays99111@naver.com>
2025-10-31 13:07:40 +0900

    프로그래머스 연속된 수의 합(120923) 추가 풀이

--- Programmers/120923.swift
+++ Programmers/120923.swift
@@ -11,3 +11,12 @@ func solution(_ num: Int, _ total: Int) -> [Int] {
     
     return answer
 }
+
+func solution2(_ num: Int, _ total: Int) -> [Int] {
+    let aver = total / num // 평균값 및 중간값
+    let offset = (num - 1) / 2 // 중간값에서 처음 혹은 끝 항까지의 거리
+    // 등차수열 첫항
+    // 공차가 1이라서 가능한 공식
+    let firstValue = aver - offset
+    return Array(firstValue..<firstValue + num)
+}
