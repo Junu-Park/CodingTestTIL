910e08ae350364f2501102d1c08a980980a8cfc9
Park Junwoo <ays99111@naver.com>
2025-11-01 17:52:13 +0900

    프로그래머스 다항식 더하기(120863) 풀이 개선

--- Programmers/120863.swift
+++ Programmers/120863.swift
@@ -1,24 +1,15 @@
 // 다항식 더하기(120863)
 
-import Foundation
-
 func solution(_ polynomial:String) -> String {
-    // components 메서드 사용을 위해서는 Foundation 프레임워크 import 필수
     let arr = polynomial.components(separatedBy: " + ")
     var varResult = 0
     var constResult = 0
     for a in arr {
-        let tmp = Array(a)
-        let lastIdx = tmp.count - 1
-
-        if tmp[lastIdx] == "x" {
-            if lastIdx == 0 {
-                varResult += 1
-            } else {
-                varResult += Int(String(tmp[0..<lastIdx]))!
-            }
+        if a.hasSuffix("x") {
+            let tmp = a.dropLast()
+            varResult += tmp.isEmpty ? 1 : Int(tmp)!
         } else {
-            constResult += Int(String(tmp))!
+            constResult += Int(a)!
         }
     }
 
