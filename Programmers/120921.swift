53bbf108441b97af708d4a104fdee8d1e035873a
Park Junwoo <ays99111@naver.com>
2025-11-02 18:11:31 +0900

    프로그래머스 문자열 밀기(120921) 풀이 개선

--- Programmers/120921.swift
+++ Programmers/120921.swift
@@ -1,23 +1,14 @@
 // 문자열 밀기(120921)
 
 func solution(_ A:String, _ B:String) -> Int {
-    var offset = 0
-    var answer = -1
     let arr = Array(A)
-    while offset < A.count {
-        if offset != 0 {
-            if String(arr[(A.count - offset)...] + arr[0..<A.count - offset]) == B {
-                answer = offset
-                break
-            }
-        } else {
-            if A == B {
-                answer = offset
-                break
-            }
+
+    let count = A.count
+    for offset in 0..<count {
+        if String(arr[(A.count - offset)...] + arr[0..<A.count - offset]) == B {
+            return offset
         }
-        offset += 1
     }
 
-    return answer
+    return -1
 }
