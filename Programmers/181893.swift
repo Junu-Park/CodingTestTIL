f055ca29964fe4b03342aedb15aacda1da89c67d
Park Junwoo <ays99111@naver.com>
2025-11-03 10:55:10 +0900

    프로그래머스 배열 조각하기(181893) 추가 풀이

--- Programmers/181893.swift
+++ Programmers/181893.swift
@@ -13,3 +13,21 @@ func solution(_ arr:[Int], _ query:[Int]) -> [Int] {
     
     return answer
 }
+
+// 위 방법은 매 for문 반복마다 배열 복사가 일어나기 때문에, O(쿼리의 길이 * 배열의 길이)
+// 아래 방법은 O(쿼리의 길이 + 최종 배열의 길이)
+func solution2(_ arr:[Int], _ query:[Int]) -> [Int] {
+    var front = 0
+    var rear = arr.count - 1
+    
+    for i in 0..<query.count {
+        if i % 2 == 0 {
+            // query는 현재 인덱스 기준이기 때문에, 원본 인덱스 기준으로 값을 구하려면 front 값을 더하기
+            rear = front + query[i]
+        } else {
+            front += query[i]
+        }
+    }
+    
+    return Array(arr[front...rear])
+}
