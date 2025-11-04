e7726c220cda5325b8eb9b83a7fee1c4ab88ee55
Park Junwoo <ays99111@naver.com>
2025-11-04 10:44:17 +0900

    프로그래머스 그림 확대(181836) 풀이 개선

--- Programmers/181836.swift
+++ Programmers/181836.swift
@@ -2,9 +2,12 @@
 
 func solution(_ picture:[String], _ k:Int) -> [String] {
     var answer: [String] = []
-    
+    // 반복적으로 발생 가능한 메모리 재할당을 방지
+    answer.reserveCapacity(picture.count * K)
     for line in picture {
         var tmp = ""
+        // 반복적으로 발생 가능한 메모리 재할당을 방지
+        answer.reserveCapacity(line.count * K)
         for char in line {
             tmp += String(repeating: char, count: k)
         }
