e801c945a5cf8fa8c757325e6a507ac5595eeaa7
Park Junwoo <ays99111@naver.com>
2025-11-04 10:40:08 +0900

    프로그래머스 그림 확대(181836) 풀이 개선

--- Programmers/181836.swift
+++ Programmers/181836.swift
@@ -4,20 +4,13 @@ func solution(_ picture:[String], _ k:Int) -> [String] {
     var answer: [String] = []
     
     for line in picture {
-        var result = ""
         var tmp = ""
         for char in line {
-            if tmp.isEmpty || tmp.last! == char {
-                tmp.append(char)
-            } else {
-                result.append(contentsOf: String(repeating: tmp, count: k))
-                tmp.removeAll()
-                tmp.append(char)
-            }
+            tmp += String(repeating: char, count: k)
         }
-        result.append(contentsOf: String(repeating: tmp, count: k))
-        for i in 0..<k {
-            answer.append(result)
+
+        for _ in 0..<k {
+            answer.append(tmp)
         }
     }
     
