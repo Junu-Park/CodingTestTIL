fc1c2385d5f08f0c9278791c3114503bad03d4ca
Park Junwoo <ays99111@naver.com>
2025-11-05 13:19:29 +0900

    프로그래머스 치킨 쿠폰(120884) 풀이 개선

--- Programmers/120884.swift
+++ Programmers/120884.swift
@@ -4,14 +4,11 @@ func solution(_ chicken:Int) -> Int {
     var answer = 0
     
     var coupon = chicken
-    var remain = 0
-    
     while (coupon / 10) > 0 {
         answer += coupon / 10
-        remain += coupon % 10
+        let remain = coupon % 10
         coupon /= 10
         coupon += remain
-        remain = 0
     }
     
     return answer
