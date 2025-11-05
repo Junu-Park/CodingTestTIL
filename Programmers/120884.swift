934b9c944801d37329ca2852da0c76851e2998da
Park Junwoo <ays99111@naver.com>
2025-11-05 14:01:02 +0900

    프로그래머스 치킨 쿠폰(120884) 추가 풀이

--- Programmers/120884.swift
+++ Programmers/120884.swift
@@ -13,3 +13,10 @@ func solution(_ chicken:Int) -> Int {
     
     return answer
 }
+
+// 쿠폰 10장 -> 치킨 1개 + 쿠폰 1장 => 실질적으로 쿠폰 9장으로 치킨 1개
+// 그래서 치킨 개수 / 9, but 교환 당시에는 쿠폰 10장으로 교환해야하기에 10장을 채우기 위한 1장을 남겨두기
+// 그래서 (치킨 개수 - 1) / 9
+func solution2(_ chicken:Int) -> Int {
+    return (chicken - 1) / 9
+}
