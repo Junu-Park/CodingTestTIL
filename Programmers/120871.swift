5d0cafbbe5e8963afe3fd0174d22f3816440ede6
Park Junwoo <ays99111@naver.com>
2025-11-08 09:38:05 +0900

    프로그래머스 저주의 숫자 3(120871) 풀이 개선

--- Programmers/120871.swift
+++ Programmers/120871.swift
@@ -8,10 +8,22 @@ func solution(_ n:Int) -> Int {
         idx += 1
         answer += 1
         
-        while Set(String(answer)).contains("3") || answer % 3 == 0 {
+        while check(answer) {
             answer += 1
         }
     }
     
     return answer
+}
+
+func check(_ num: Int) -> Bool {
+    if num % 3 == 0 { return true }
+
+    var tmp = num
+    while tmp > 0 {
+        if tmp % 10 == 3 { return true }
+        tmp /= 10
+    }
+
+    return false
 }
