97a147f83c22ec486d9bf269b08fa1dd547223db
Park Junwoo <ays99111@naver.com>
2025-11-09 18:55:17 +0900

    프로그래머스 콜라츠 추측(12943) 풀이 개선

--- Programmers/12943.swift
+++ Programmers/12943.swift
@@ -1,27 +1,26 @@
 // 콜라츠 추측(12943)
 
-func solution(_ num:Int) -> Int {
-    var cnt = 0
-    var result = Int64(num)
-    
+func solution(_ num: Int) -> Int {
     if num == 1 {
         return 0
     }
-    
-    while cnt <= 500 {
+
+    var cnt = 0
+    // 오버플로우 방지를 위한 Int64 타입 캐스팅
+    var result = Int64(num)
+
+    while cnt < 500 {
         if result % 2 == 0 {
-            result /= 2 
+            result /= 2
         } else {
             result *= 3
             result += 1
         }
-        
+
         cnt += 1
-        
-        if result == 1 {
-            break
-        }
+
+        if result == 1 { return cnt }
     }
-    
-    return result == 1 ? cnt : -1
+
+    return -1
 }
