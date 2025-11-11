0f1abc339543137c67914b68ce24b2f57f0a3bcd
Park Junwoo <ays99111@naver.com>
2025-11-11 15:12:20 +0900

    백준 별 찍기 - 17(10992) 풀이 주석 추가

--- BaekJoon/10992.swift
+++ BaekJoon/10992.swift
@@ -4,18 +4,20 @@ let n = Int(readLine()!)!
 
 var arr = Array(repeating: Array(repeating: " ", count: n * 2 - 1), count: n)
 
+let center = n - 1
+
+// 마지막 줄 별 채우기
 for i in arr[center].indices {
     arr[n - 1][i] = "*"
 }
 
-let center = n - 1
-
-for a in 0 ..< (n - 1) {
-    arr[a][center - a] = "*"
-    arr[a][center + a] = "*"
+// 나머지 줄 별 채우기
+for i in 0 ..< (n - 1) {
+    arr[i][center - i] = "*"
+    arr[i][center + i] = "*"
 }
 
 for i in arr.indices {
-    // 별 뒤의 필요 없는 공백은 제거해야한다.
+    // 라인에서 마지막 별 뒤의 필요 없는 공백은 제거하고 출력해야한다.
     print(arr[i][0 ... center + i].joined())
 }
