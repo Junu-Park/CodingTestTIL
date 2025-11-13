ec133de28dbea64c7ea157f4ae88e4e9f0d35550
Park Junwoo <ays99111@naver.com>
2025-11-13 14:45:53 +0900

    백준 좌표 정렬하기(11650) 풀이 개선

--- BaekJoon/11650.swift
+++ BaekJoon/11650.swift
@@ -4,20 +4,16 @@ let n = Int(readLine()!)!
 
 typealias Point = (x: Int, y: Int)
 
-var board = Array(repeating: Point(x: 0, y: 0), count: n)
+var board = Array<Point>()
+board.reserveCapacity(n)
 
-for i in board.indices {
+for _ in 0..<n {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
-    board[i] = Point(x: input[0], y: input[1])
+    board.append(Point(x: input[0], y: input[1]))
 }
 
-board.sort { fir, sec in 
-    if fir.x != sec.x {
-        return fir.x < sec.x
-    } else {
-        return fir.y < sec.y
-    }
-}
+// 튜플 비교 연산자 활용(6개 요소까지 지원): 왼쪽 요소부터 차례대로 비교하면서 다른 요소에서 결정
+board.sort { $0 < $1 } // sort(by: <)
 
 for p in board {
     print("\(p.x) \(p.y)")
