5c01fefdbef4469283a08b0db5f2de898da5e9ad
Park Junwoo <ays99111@naver.com>
2025-11-16 16:25:40 +0900

    백준 알파벳 거리(5218) 풀이 개선

--- BaekJoon/5218.swift
+++ BaekJoon/5218.swift
@@ -4,20 +4,15 @@ let n = Int(readLine()!)!
 
 for _ in 0..<n {
     let input = readLine()!.split(separator: " ")
-    let count = input[0].count
-    var a = Array(input[0])
-    a.reserveCapacity(count)
-    var b = Array(input[1])
-    b.reserveCapacity(count)
     var result: [Int] = []
-    result.reserveCapacity(count)
-    for i in 0..<count {
-        let aChar = Int(a[i].asciiValue!)
-        let bChar = Int(b[i].asciiValue!)
-        if bChar >= aChar {
-            result.append(bChar - aChar)
+    result.reserveCapacity(input[0].count)
+    for (aChar, bChar) in zip(input[0], input[1]) {
+        let aVal = Int(aChar.asciiValue!) // aChar.unicodeScalars.first!.value
+        let bVal = Int(bChar.asciiValue!) // bChar.unicodeScalars.first!.value
+        if bVal >= aVal {
+            result.append(bVal - aVal)
         } else {
-            result.append((bChar + 26) - aChar)
+            result.append((bVal + 26) - aVal)
         }
     }
 
