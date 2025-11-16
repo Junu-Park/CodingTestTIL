40ae4cf010207dc50c7aa3a19f0cc966b53ab2a1
Park Junwoo <ays99111@naver.com>
2025-11-16 14:42:34 +0900

    백준 문자열 분석(10820) 풀이 개선

--- BaekJoon/10820.swift
+++ BaekJoon/10820.swift
@@ -1,14 +1,13 @@
 // 문자열 분석(10820)
 
 while let input = readLine() {
-    var arr = input.map { $0 }
-    
     var lower = 0
     var upper = 0
     var number = 0
     var whitespace = 0
-    for char in arr {
-        if Int(String(char)) != nil {
+    
+    for char in input {
+        if char.isNumber {
             number += 1
         } else if char == " " {
             whitespace += 1
