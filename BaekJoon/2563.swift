f7e4d556d08808b3a54ddc01bc44ca879529aec4
Park Junwoo <ays99111@naver.com>
2025-11-17 16:29:06 +0900

    백준 색종이(2563) 풀이 개선

--- BaekJoon/2563.swift
+++ BaekJoon/2563.swift
@@ -1,7 +1,7 @@
 // 색종이(2563)
 
 let n = Int(readLine()!)!
-
+var answer = 0
 var board = Array(repeating: Array(repeating: false, count: 100 + 1), count: 100 + 1)
 for _ in 0..<n {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
@@ -13,16 +13,9 @@ for _ in 0..<n {
             if board[i][j] { continue }
 
             board[i][j] = true
-        }
-    }
-}
-
-var answer = 0
-for i in 1...100 {
-    for j in 1...100 {
-        if board[i][j] {
             answer += 1
         }
     }
 }
+
 print(answer)
