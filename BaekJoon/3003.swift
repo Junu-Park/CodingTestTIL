0e909162f02ebada37c887cc4697e09849e388d6
Park Junwoo <ays99111@naver.com>
2025-11-17 10:19:47 +0900

    백준 킹, 퀸, 룩, 비숍, 나이트, 폰(3003) 추가 풀이

--- BaekJoon/3003.swift
+++ BaekJoon/3003.swift
@@ -12,3 +12,10 @@ for i in 0..<6 {
 }
 
 print(black.joined(separator: " "))
+
+// 간결한 풀이
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+let white = [1,1,2,2,2,8]
+
+print(zip(white, input).map { String($0 - $1) }.joined(separator: " "))
