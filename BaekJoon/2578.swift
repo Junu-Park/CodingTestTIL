9b07c80b1145e55a080b4f441343a9198a96ed3c
Park Junwoo <ays99111@naver.com>
2025-11-24 15:29:00 +0900

    백준 빙고(2578) 풀이

---
+++ BaekJoon/2578.swift
@@ -0,0 +1,68 @@
+// 빙고(2578)
+
+var board: [[Int]] = []
+board.reserveCapacity(5)
+
+for _ in 0 ..< 5 {
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+    board.append(input)
+}
+
+var vis = Array(repeating: Array(repeating: false, count: 5), count: 5)
+
+var count = 0
+
+var answer = 0
+
+for _ in 0 ..< 5 {
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+    if answer != 0 { continue }
+
+    for i in input {
+        
+        count += 1
+        var result = 0
+        
+        outLoop: for y in 0 ..< 5 {
+            for x in 0 ..< 5 {
+                if board[y][x] == i {
+                    vis[y][x] = true
+                    break outLoop
+                }
+            }
+        }
+
+        // 가로 확인
+        for y in 0 ..< 5 {
+            if vis[y].allSatisfy { $0 } {
+                result += 1
+            }
+        }
+
+        // 세로 확인
+        for x in 0 ..< 5 {
+            if (0...4).allSatisfy { vis[$0][x]} {
+                result += 1
+            }
+        }
+
+        // 좌측대각선 확인
+        if (0...4).allSatisfy { vis[$0][$0] } {
+            result += 1
+        }
+
+        // 우측대각선 확인
+        if (0...4).allSatisfy { vis[$0][4 - $0] } {
+            result += 1
+        }
+
+        if result >= 3 {
+            answer = count
+            break
+        }
+    }
+}
+
+print(answer)
