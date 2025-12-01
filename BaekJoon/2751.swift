7636bfbfa6ce0496ca7654e27e0b478d75460265
Park Junwoo <ays99111@naver.com>
2025-12-01 14:20:30 +0900

    백준 수 정렬하기 2(2751) 풀이 개선

--- BaekJoon/2751.swift
+++ BaekJoon/2751.swift
@@ -11,6 +11,9 @@ for _ in 0..<n {
 
 arr.sort()
 
+// 출력 방식1: 메모리 사용 감소(매 출력 시, 버퍼 제거), 시간 증가(매 출력마다, 시스템 콜)
 for a in arr {
     print(a)
 }
+// 출력 방식2: 시간 감소(한 번의 시스템 콜), 메모리 사용 증가(버퍼를 모아서 한 번에 전송)
+print(arr.map { String($0) }.joined(separator: "\n"))
