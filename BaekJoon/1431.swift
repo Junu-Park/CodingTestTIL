b88ee682326760ffaa2f74fb36d2a121685b5b4e
Park Junwoo <ays99111@naver.com>
2025-12-04 14:55:20 +0900

    백준 시리얼 번호(1431) 풀이 개선

--- BaekJoon/1431.swift
+++ BaekJoon/1431.swift
@@ -2,26 +2,27 @@
 
 let n = Int(readLine()!)!
 
-var serial: [String] = []
+var serial: [(code: String, cnt: Int, sum: Int)] = []
 
 for _ in 0..<n {
-    serial.append(readLine()!)
+    let code = readLine()!
+    let cnt = code.count
+    let sum = code.reduce(0) { $0 + ($1.isNumber ? Int(String($1))! : 0) }
+    serial.append((code, cnt, sum))
 }
 
 serial.sort { fir, sec in
-    if fir.count != sec.count {
-        return fir.count < sec.count
+    if fir.cnt != sec.cnt {
+        return fir.cnt < sec.cnt
     } else {
-        let firSum = fir.filter { $0.isNumber }.reduce(0) { $0 + Int(String($1))! }
-        let secSum = sec.filter { $0.isNumber }.reduce(0) { $0 + Int(String($1))! }
-        if firSum != secSum {
-            return firSum < secSum
+        if fir.sum != sec.sum {
+            return fir.sum < sec.sum
         } else {
-            return fir < sec
+            return fir.code < sec.code
         }
     }
 }
 
 for ser in serial {
-    print(ser)
+    print(ser.code)
 }
