dfceb42d11e0f1b11a04cbfd7ea390d7d45ba7df
Park Junwoo <ays99111@naver.com>
2025-12-05 17:53:06 +0900

    백준 패션왕 신해빈(9375) 풀이

---
+++ BaekJoon/9375.swift
@@ -0,0 +1,19 @@
+// 패션왕 신해빈(9375)
+
+let caseN = Int(readLine()!)!
+
+for _ in 0..<caseN {
+    let n = Int(readLine()!)!
+
+    // 의상 이름의 중복은 없기 때문에, 종류별로 몇 별 있는지만 체크하면 됨
+    var closet = [String: Int]()
+    for _ in 0..<n {
+        let input = readLine()!.split(separator: " ").map { String($0) }
+        closet[input[1], default: 0] += 1
+    }
+    
+    // 각 종류별로 입지 않는 경우도 있으니 종류별로 +1, 알몸인 경우는 없으니 - 1
+    let answer = closet.reduce(1) { $0 * ($1.value + 1) } - 1
+
+    print(answer)
+}
