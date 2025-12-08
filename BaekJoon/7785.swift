6386eb82110449a5b76da1ff91721e7e1d944e81
Park Junwoo <ays99111@naver.com>
2025-12-08 14:30:16 +0900

    백준 회사에 있는 사람(7785) 풀이 개선

--- BaekJoon/7785.swift
+++ BaekJoon/7785.swift
@@ -2,17 +2,18 @@
 
 let n = Int(readLine()!)!
 
-var record = [String: Bool]()
+var record = Set<String>()
 
 for _ in 0..<n {
     let input = readLine()!.split(separator: " ").map { String($0) }
+    let name = input[0]
     if input[1] == "enter" {
-        record[input[0], default: false] = true
+        record.insert(name)
     } else {
-        record.removeValue(forKey: input[0])
+        record.remove(name)
     }
 }
 
-for r in record.sorted { $0.key > $1.key } {
-    print(r.key)
+for r in record.sorted(by: >) {
+    print(r)
 }
