71cc9f94864b8d06399822ba222ea6441293d95c
Park Junwoo <ays99111@naver.com>
2025-12-11 13:46:52 +0900

    백준 싸이버개강총회(19583) 풀이 개선

--- BaekJoon/19583.swift
+++ BaekJoon/19583.swift
@@ -1,23 +1,24 @@
 // 싸이버개강총회(19583)
 
+func convertToInt(_ str: String) -> Int {
+    let tmp = str.split(separator: ":").map { Int($0)! }
+    return tmp[0] * 60 + tmp[1]
+}
+
 let times = readLine()!.split(separator: " ").map { String($0) }
-let s = times[0].split(separator: ":").map { Int($0)! }
-let sInt = s[0] * 60 + s[1]
-let e = times[1].split(separator: ":").map { Int($0)! }
-let eInt = e[0] * 60 + e[1]
-let q = times[2].split(separator: ":").map { Int($0)! }
-let qInt = q[0] * 60 + q[1]
+let s = convertToInt(times[0])
+let e = convertToInt(times[1])
+let q = convertToInt(times[2])
 
 var result = [String: Bool]()
 while let record = readLine() {
     let tmp = record.split(separator: " ").map { String($0) }
-    let time = tmp[0].split(separator: ":").map { Int($0)! }
-    let timeInt = time[0] * 60 + time[1]
+    let t = convertToInt(tmp[0])
     let name = tmp[1]
 
-    if timeInt <= sInt {
+    if t <= s {
         result[name] = true 
-    } else if (eInt...qInt).contains(timeInt) {
+    } else if (e...q).contains(t) {
         if let _ = result[name] {
             result[name] = false
         }
