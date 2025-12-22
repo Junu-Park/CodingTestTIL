03e169091720245e5031c142455cbb6a3c59284f
Arthur <ays99111@naver.com>
2025-12-22 17:35:21 +0900

    백준 방 배정(13300) 풀이 개선

--- BaekJoon/13300.swift
+++ BaekJoon/13300.swift
@@ -1,43 +1,19 @@
 // 방 배정(13300)
 
 let nk = readLine()!.split(separator: " ").map { Int($0)! }
-
 let n = nk[0]
 let k = nk[1]
 
-var students = [(gender: Int, grade: Int)]()
+var students = [String: Int]()
 
 for _ in 0..<n {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    students.append((input[0], input[1]))
+    students[readLine()!, default: 0] += 1
 }
 
-students.sort { fir, sec in
-    if fir.grade != sec.grade {
-        return fir.grade < sec.grade
-    } else {
-        return fir.gender < sec.gender
-    }
+var answer = 0
+for val in students.values {
+    answer += val / k
+    answer += val % k == 0 ? 0 : 1
 }
 
-var rooms = [Int]()
-var cnt = 0
-
-for idx in students.indices {
-    if cnt == 0 {
-        cnt += 1
-    } else {
-        let prev = students[idx - 1]
-        let cur = students[idx]
-        if cnt == k || cur.gender != prev.gender || cur.grade != prev.grade {
-            rooms.append(cnt)
-            cnt = 1
-        } else {
-            cnt += 1
-        }
-    }
-}
-
-rooms.append(cnt)
-
-print(rooms.count)
+print(answer)
