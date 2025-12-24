28ca2bf7838339ef66717d7d9f8d6729201d9c35
Park Junwoo <ays99111@naver.com>
2025-12-24 17:01:05 +0900

    백준 요세푸스 문제(1158) 풀이 개선 및 주석 추가

--- BaekJoon/1158.swift
+++ BaekJoon/1158.swift
@@ -2,6 +2,7 @@
 
 let nk = readLine()!.split(separator: " ").map { Int($0)! }
 let n = nk[0]
+// 인덱스로 활용하기 위해, - 1
 let k = nk[1] - 1
 
 var people = Array(1...n).map { String($0) }
@@ -11,14 +12,11 @@ var answer = [String]()
 answer.reserveCapacity(n)
 
 while !people.isEmpty {
+    // (현재 인덱스 + 타겟 인덱스까지의 거리) % 현재 배열의 크기
     idx = (idx + k) % people.count
 
     answer.append(people[idx])
     people.remove(at: idx)
-
-    if idx == people.count {
-        idx = 0
-    }
 }
 
 print("<\(answer.joined(separator: ", "))>")
