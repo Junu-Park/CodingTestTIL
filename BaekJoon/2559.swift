0e4f30bc0befaca68eeeea953d669994118e0624
Park Junwoo <ays99111@naver.com>
2025-12-25 21:52:19 +0900

    백준 수열(2559) 풀이 개선

--- BaekJoon/2559.swift
+++ BaekJoon/2559.swift
@@ -1,24 +1,17 @@
 // 수열(2559)
 
 let nk = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nk[0]
-let k = nk[1]
-
-var result = 0
-
-var answer = -100 * n - 1
-
+let (n, k) = (nk[0], nk[1])
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 
-for i in 0..<n {
+var result = input[0..<k].reduce(0, +)
+
+var answer = result
+
+for i in k..<n {
     result += input[i]
-    
-    if i == (k - 1) {
-        answer = max(answer, result)
-    } else if i > (k - 1) {
-        result -= input[i - k]
-        answer = max(answer, result)
-    }
+    result -= input[i - k]
+    answer = max(answer, result)
 }
 
 print(answer)
