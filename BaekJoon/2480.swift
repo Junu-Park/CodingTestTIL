3d194762ad23746819d7992bcc073cc50aba1d33
Arthur <ays99111@naver.com>
2026-01-03 21:26:42 +0900

    백준 주사위 세개(2480) 풀이 개선

--- BaekJoon/2480.swift
+++ BaekJoon/2480.swift
@@ -1,19 +1,15 @@
 // 주사위 세개(2480)
 
 let dices = readLine()!.split(separator: " ").map { Int($0)! }
-let dicesSet = Set(dices)
+var dicesDict = [Int:Int]()
+for dice in dices {
+    dicesDict[dice, default: 0] += 1
+}
 
-if dicesSet.count == 1 {
+if dicesDict.count == 1 {
     print(10000 + dices.first! * 1000)
-} else if dicesSet.count == 2 {
-    var target = 0
-    for dice in dicesSet {
-        if dices.filter { $0 == dice }.count == 2 {
-            target = dice
-            break
-        }
-    }
-    print(1000 + target * 100)
+} else if dicesDict.count == 2 {
+    print(1000 + dicesDict.first { $0.value == 2 }!.key * 100)
 } else {
     print(dices.max()! * 100)
 }
